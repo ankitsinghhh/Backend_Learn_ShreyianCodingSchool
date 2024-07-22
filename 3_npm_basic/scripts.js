ef3fddff80c652d23767ee5f7fd8d490c7adff27
ankitsinghhh <ankitsingh79834@gmail.com>
2024-07-22 22:37:49 +0530

    lecture video 3 completed

---
+++ 3_npm_basic/scripts.js
@@ -0,0 +1,21 @@
+//-------------------npm - node package manager - initially named as this 
+// later many packages other than node introduced so it only came to be known as npm only
+// node js core me install aata h use kehte h module
+//jo npm se install hota h use package kehte h 
+
+
+//-------------------------install and uninstall --------------------
+
+// install => npm install one-liner-joke
+//uninstall => npm uninstall one-liner-joke
+// to install particular version -> npm install one-liner-joke@3.0.9
+
+//------------------------understanding node modules ---------------
+
+// node module => is a file which contains javascript code, it has some predefined properties and methods.
+// dependencies -> packagges and packages ki dependencies
+// devdependencies -> aise packages jo sirf development me kaam aenge pr jab app bn k upload hojaega tb in packages ko use nhi kr rhe honge
+
+//  note: jo commands khud se create krte h , usme run likhna pdta h ,
+// example: npm run start , npm run dev
+//while npm start is already present
